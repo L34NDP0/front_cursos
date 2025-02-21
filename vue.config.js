e5ce88b53eb967b0a65e915d30759eb58a3eddda
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2025-02-21 08:46:09 -0300

    Updatevue
    novo update vue

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,11 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   devServer: {
     port: process.env.PORT || 8080,
     allowedHosts: "all",
   },
-
   css: {
     loaderOptions: {
       sass: {
